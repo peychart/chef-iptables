4d3d60981c108065d418712783db62f38c6bed2f
Philippe EYCHART <philippe.eychart@informatique.gov.pf>
2014-11-28 11:38:33 -1000

    default correction

--- attributes/default.rb
+++ attributes/default.rb
@@ -28,8 +28,8 @@ default['chef-iptables']['rules']['default'] = [
 ]
 
 default['chef-iptables']['ipv4rules']['filter']['INPUT']['ssh'] = '--protocol tcp --dport 22 --sport 1024:65535 --match state --state NEW --jump ACCEPT'
-default['chef-iptables']['ipv4rules']['nat']['OUTPUT']['dns'] = '--protocol udp --dport 53 --jump ACCEPT'
-default['chef-iptables']['ipv4rules']['nat']['OUTPUT']['ntp'] = '--protocol udp --dport 123 --jump ACCEPT'
+default['chef-iptables']['ipv4rules']['filter']['OUTPUT']['dns'] = '--protocol udp --dport 53 --jump ACCEPT'
+default['chef-iptables']['ipv4rules']['filter']['OUTPUT']['ntp'] = '--protocol udp --dport 123 --jump ACCEPT'
 default['chef-iptables']['ipv4rules']['filter']['FORWARD']['default'] = nil
 
 default['chef-iptables']['ipv4rules']['nat']['PREROUTING']['default'] = nil
